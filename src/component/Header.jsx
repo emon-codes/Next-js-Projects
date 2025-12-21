0496b68c8edbd94d2dfd9a7331207dc0a84e3b58
EMON AHMED RANA <emon9996a@gmail.com>
2025-12-21 22:23:12 +0600

    header add

---
+++ src/component/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center text-xl mx-auto container">
+        <div className="font-semibold text-2xl text-red-800">EMON</div>
+       
+              <div className="flex flex-row gap-3">
+          <Link href="/">Home</Link>
+          <Link href="/about">About Us</Link>
+          <Link href="/contact">Contact Us</Link>
+
+          <Link href="/blog">Blog</Link>
+              </div>
+              <div>
+                  <button className="btn border">View more</button>
+              
+              
+              </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
