5b3e003b7c5e1b44dd1b00913bc46b69f722e1e3
EMON AHMED RANA <emon9996a@gmail.com>
2025-12-22 21:33:08 +0600

    react icon install

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/component/Header";
+import Footer from "@/component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       >
         <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
